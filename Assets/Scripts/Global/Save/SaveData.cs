5d0dfc6f6d56f83a53a2208583dda6d9d4606d6c
raihan <raihan441@gmail.com>
2022-09-08 16:56:27 +0800

    Finishing

--- Assets/Scripts/Global/Save/SaveData.cs
+++ Assets/Scripts/Global/Save/SaveData.cs
@@ -52,7 +52,6 @@ namespace TriviaGame.Global.Save
         {
             string json = JsonUtility.ToJson(this);
             PlayerPrefs.SetString(_prefsKey, json);
-            Debug.Log(json);
         }
 
         private void Load()
